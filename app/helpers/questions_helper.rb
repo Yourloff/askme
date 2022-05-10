249600d51adbb62ebb085e5ec3e88493e32e2504
yourloff <alex.yourloff@gmail.com>
2022-05-10 16:31:35 +0700

    simplification with helpers

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def author_present(author)
+    if author.present?
+      link_to "От #{ display_nickname(author) }", user_path(author)
+    else
+      'Анонимный вопрос'
+    end
+  end
 end
