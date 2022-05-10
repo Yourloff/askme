f3e23af5b6ae611dae8af6c6c2373da39b2d3af1
yourloff <alex.yourloff@gmail.com>
2022-05-11 06:05:35 +0700

    all conditions in helpers

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,6 +1,8 @@
 module QuestionsHelper
   def author_present(author)
-    if author.present?
+    if author == current_user && current_user.present?
+      'От меня'
+    elsif author.present?
       link_to "От #{ display_nickname(author) }", user_path(author)
     else
       'Анонимный вопрос'
