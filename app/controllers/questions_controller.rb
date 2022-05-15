7515978c0dd180200d5ceb48c9f2eaade4b8ae88
yourloff <alex.yourloff@gmail.com>
2022-05-15 18:22:37 +0700

    add id @user

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,6 +7,7 @@ class QuestionsController < ApplicationController
 
     @question = Question.new(question_params)
     @question.author = current_user
+    @user = User.find(question_params[:user_id])
 
     if @question.save
       redirect_to user_path(@question.user), notice: 'Новый вопрос создан!'
