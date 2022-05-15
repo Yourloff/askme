acc21e72d960a07b9585d0911c5fc83e4bfde9d2
yourloff <alex.yourloff@gmail.com>
2022-05-15 09:15:46 +0700

    const email validate

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,5 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
 
-  validates :body, length: { maximum: 280 }, presence: true
+  validates :body, length: { maximum: 280 }, allow_blank: false, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
   REG_NICKNAME = /\A[a-zA-Z0-9_]*\Z/
   REG_COLOR = /\A#[a-f0-9]{6}\z/i
+  REG_EMAIL = /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
   has_secure_password
   has_many :questions, dependent: :delete_all
   has_many :authored_questions, class_name: 'Question', foreign_key: 'author_id', dependent: :nullify
 
   after_validation :downcase_nickname
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: REG_EMAIL }
   validates :nav_color, format: { with: REG_COLOR }
   validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: REG_NICKNAME }
-  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
   include Gravtastic
   gravtastic(secure: true, filetype: :png, size: 100, default: 'retro')
