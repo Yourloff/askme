2e801b2176b426ff9b3c8af4307027d6a71acfa0
yourloff <alex.yourloff@gmail.com>
2022-05-25 11:55:42 +0700

    Two migrations in one

--- db/migrate/20220525032404_create_quest_tags.rb
+++ db/migrate/20220525032404_create_quest_tags.rb
@@ -1,5 +1,9 @@
 class CreateQuestTags < ActiveRecord::Migration[7.0]
   def change
+    create_table :tags do |t|
+      t.string :name
+    end
+
     create_table :quest_tags do |t|
       t.references :question, null: false, foreign_key: true
       t.references :tag, null: false, foreign_key: true
--- db/migrate/20220525035554_create_tags.rb
+++
@@ -1,7 +0,0 @@
-class CreateTags < ActiveRecord::Migration[7.0]
-  def change
-    create_table :tags do |t|
-      t.string :name
-    end
-  end
-end
