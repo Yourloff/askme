27a2ee86813ec42c885f9917d40f7d92a1f6c513
Alexey Orloff <alex.yourloff@gmail.com>
2022-07-25 17:11:11 +0700

    fix tags&add migration index

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_08_152309) do
-  create_table "hashtags", force: :cascade do |t|
-    t.string "hashtag"
-    t.integer "questions_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["questions_id"], name: "index_hashtags_on_questions_id"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_07_25_094234) do
   create_table "quest_tags", force: :cascade do |t|
     t.integer "question_id", null: false
     t.integer "tag_id", null: false
@@ -39,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_08_152309) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,7 +46,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_08_152309) do
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
-  add_foreign_key "hashtags", "questions", column: "questions_id"
   add_foreign_key "quest_tags", "questions"
   add_foreign_key "quest_tags", "tags"
   add_foreign_key "questions", "users", column: "author_id"
